917536f0fea0f696bc6daa4a5e6c168ee50d00fa
DONG Yifang <ydong@murex.com>
2025-01-09 06:39:13 +0100

    refactoring

--- src/test/java/example/SessionLayerBasicTest.java
+++ src/test/java/example/SessionLayerBasicTest.java
@@ -1,4 +1,12 @@
+/**
+ *  Copyright Murex S.A.S., 2003-2025. All Rights Reserved.
+ *
+ *  This software program is proprietary and confidential to Murex S.A.S and its affiliates ("Murex") and, without limiting the generality of the foregoing reservation of rights, shall not be accessed, used, reproduced or distributed without the
+ *  express prior written consent of Murex and subject to the applicable Murex licensing terms. Any modification or removal of this copyright notice is expressly prohibited.
+ */
 package example;
+
+import java.util.concurrent.Executors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
@@ -8,7 +16,7 @@ class SessionLayerBasicTest {
 
     @Test
     public void the_basic_test() {
-        Service service = new SessionLayer();
+        Service service = new SessionLayer(Runnable::run);
         MyTestClient client = new MyTestClient();
         client.subscribe(service);
 
@@ -19,7 +27,7 @@ class SessionLayerBasicTest {
 
     @Test
     public void thread_sleep_solution() throws InterruptedException {
-        Service service = new SessionLayer();
+        Service service = new SessionLayer(Executors.newFixedThreadPool(1));
         MyTestClient client = new MyTestClient();
         client.subscribe(service);
 
